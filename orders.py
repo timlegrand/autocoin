8d4fa90887a7ea260e38b7b04bf3e222236a9858
Tim Legrand <timlegrand.perso+dev@gmail.com>
2017-07-28 00:01:56 +0200

    feat: tabulate output

--- orders.py
+++ orders.py
@@ -1,9 +1,17 @@
 import request
 import json
+import tabulate
+import datetime
 
 
 if __name__ == '__main__':
     open_orders = request.request('open orders')
+    table=[]
     for k, v in open_orders['result']['open'].items():
-        print(30 * '=' + '\nOrder ' + k + ':')
-        print(json.dumps(v, indent=4))  # json prettier than pprint, even though 'v' is pure Python
+        iso_date = datetime.datetime.fromtimestamp(int(v['opentm'])).strftime('%Y-%m-%d_%H:%M:%S')
+        order_line = (' '.join([k, v['descr']['order'], iso_date])).split()
+        table.append(order_line)
+
+    sorted_table = sorted(table, key=lambda x: x[-1], reverse=True)
+    table_headers=['Order \#', '', 'Volume', 'Pair', '', '', 'Cost', 'Created']
+    print(tabulate.tabulate(sorted_table, headers=table_headers))
