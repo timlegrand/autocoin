32ba41907d5850b4dc080616d576b792850e52c3
Tim Legrand <timlegrand.perso+dev@gmail.com>
2017-08-08 00:59:38 +0200

    feat: sort account balance table

--- src/data/account-balance.py
+++ src/data/account-balance.py
@@ -6,12 +6,10 @@ import datetime
 
 if __name__ == '__main__':
     balance = request.request('account balance')
-    table=[]
-    for k, v in balance['result']['open'].items():
-        iso_date = datetime.datetime.fromtimestamp(int(v['opentm'])).strftime('%Y-%m-%d_%H:%M:%S')
-        order_line = (' '.join([k, v['descr']['order'], iso_date])).split()
-        table.append(order_line)
+    table = balance['result']
+    del table['KFEE']  # Kraken Fee Credit
 
-    sorted_table = sorted(table, key=lambda x: x[-1], reverse=True)
-    table_headers=['Order \#', '', 'Volume', 'Pair', '', '', 'Cost', 'Created']
-    print(tabulate.tabulate(sorted_table, headers=table_headers))
+    # sorted_table = sorted(table.items(), key=lambda x: float(x[-1]), reverse=True)  # By balance
+    sorted_table = sorted(table.items())  # By currency
+    table_headers=['Currency', 'Balance']
+    print(tabulate.tabulate(sorted_table, headers=table_headers, floatfmt=".9f"))
