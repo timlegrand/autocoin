7b3b21548d36540b0073ed1367e51af953ca6c8e
Tim Legrand <timlegrand.perso+dev@gmail.com>
2017-08-08 10:51:52 +0200

    feat: improve orders printing

--- src/data/orders.py
+++ src/data/orders.py
@@ -9,8 +9,9 @@ if __name__ == '__main__':
     for k, v in open_orders['result']['open'].items():
         iso_date = datetime.datetime.fromtimestamp(int(v['opentm'])).strftime('%Y-%m-%d_%H:%M:%S')
         order_line = (' '.join([k, v['descr']['order'], iso_date])).split()
+        del order_line[4]
         table.append(order_line)
 
     sorted_table = sorted(table, key=lambda x: x[-1], reverse=True)
-    table_headers=['Order \#', '', 'Volume', 'Pair', '', '', 'Cost', 'Created']
+    table_headers=['Order #', 'Type', 'Volume', 'Pair', 'Method', 'Cost', 'Created']
     print(tabulate.tabulate(sorted_table, headers=table_headers))
