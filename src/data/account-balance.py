0ed364cace6bffe4c0bc3f3ff17568dbb764840d
Tim Legrand <timlegrand.perso+dev@gmail.com>
2017-08-09 23:45:09 +0200

    refactor(balance): insulate queries into a dedicated function

--- src/data/account-balance.py
+++ src/data/account-balance.py
@@ -1,12 +1,17 @@
 from connectors import request
-import tabulate
 
-
-if __name__ == '__main__':
+def get_account_balance():
     balance = request.request('account balance')
     del balance['KFEE']  # Kraken Fee Credit
 
+    table_headers=['Currency', 'Balance']
+
+    return balance, table_headers
+
+
+if __name__ == '__main__':
+    balance, table_headers = get_account_balance()
     # sorted_table = sorted(balance.items(), key=lambda x: float(x[-1]), reverse=True)  # By balance
     sorted_table = sorted(balance.items())  # By currency
-    table_headers=['Currency', 'Balance']
+    import tabulate
     print(tabulate.tabulate(sorted_table, headers=table_headers, floatfmt=".9f"))
