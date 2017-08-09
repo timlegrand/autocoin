cb3e7e1360e3b08980dbdfb2c99764b7fc2e8376
Tim Legrand <timlegrand.perso+dev@gmail.com>
2017-08-09 08:44:40 +0200

    refactor: rename cap into balance-cap
    because of incoming placed-orders-cap
    includes minor style changes

--- src/metrics/balance-capitalization.py
+++ src/metrics/balance-capitalization.py
@@ -3,31 +3,29 @@ import tabulate
 
 
 if __name__ == '__main__':
-    # Get balance for all owned currencies
+
+    # Get account balance for all owned currencies
     balance = request.request('account balance')
     del balance['KFEE']  # Kraken Fee Credit
     del balance['ZEUR']
-
-    # sorted_balance = sorted(table.items(), key=lambda x: float(x[-1]), reverse=True)  # By balance
-    # sorted_balance = sorted(balance.items())  # By currency
-    table_headers=['Currency', 'Balance']
-
     owned_currencies = balance.keys()
 
+    # Get ticker for owned currencies
     pairs_list = [x + 'ZEUR' for x in owned_currencies if x not in ['BCH']]
     pairs_list.extend([x + 'XXBT' for x in owned_currencies if x not in ['XXBT', 'BCH']])
     pairs_string = ', '.join(pairs_list)
-
     ticker = request.request('ticker', {'pair': pairs_string})
 
-    table = {}
+    # Consolidate the balance capitalization table
+    cap_table = {}
+    total_dir_cap = 0
+    total_xbt_cap = 0
     xbt_eur_ask = float(ticker['XXBTZEUR']['a'][0])
     for pair in ticker:
         source_curr = pair[:4]
-        if table:
-            if source_curr in table:
+        if cap_table:
+            if source_curr in cap_table:
                 # currency already processed in a previous iteration
-                print('skipping pair: ' + pair)
                 continue
         dest_curr = pair[4:]
         curr_balance = float(balance[source_curr])
@@ -39,9 +37,9 @@ if __name__ == '__main__':
             curr_ask_xbt = 1
         curr_xbt_cap = curr_ask_xbt * curr_balance * xbt_eur_ask
         result_line = (source_curr, curr_balance, curr_ask_eur, curr_dir_cap, curr_ask_xbt, curr_xbt_cap)            
-        table[source_curr] = result_line
+        cap_table[source_curr] = result_line
 
-    table = [list(table[x]) for x in table]
-    sorted_table = sorted(table, key=lambda x: x[-1], reverse=True)
+    cap_table = [list(cap_table[x]) for x in cap_table]
+    sorted_table = sorted(cap_table, key=lambda x: x[-1], reverse=True)
     table_headers=['Currency', 'Balance', 'Ask (EUR)', 'Direct Cap.', 'Ask (XBT)', 'XBT-to-EUR Cap.']
     print(tabulate.tabulate(sorted_table, headers=table_headers, floatfmt=".5f"))
