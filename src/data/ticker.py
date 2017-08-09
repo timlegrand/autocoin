30984b9ebfcaf557445a829cd0924c9ff8a50d17
Tim Legrand <timlegrand.perso+dev@gmail.com>
2017-08-09 23:36:08 +0200

    refactor(ticker): insulate queries into a dedicated function

--- src/data/ticker.py
+++ src/data/ticker.py
@@ -1,16 +1,22 @@
 from connectors import request
-import tabulate
 
 
-if __name__ == '__main__':
-    ticker = request.request('ticker', {'pair': 'XZECXXBT'})
-    import json
+def get_ticker(pairs=['XXBTZEUR']):
+    ticker = request.request('ticker', {'pair': ', '.join(pairs)})
     
     table=[]
     for k, v in ticker.items():
         ticker_line = [k, v['b'][0], v['a'][0]]
         table.append(ticker_line)
 
-    sorted_table = sorted(table, key=lambda x: x[-1], reverse=True)
     table_headers=['Pair', 'Bid', 'Ask']
+
+    return table, table_headers
+
+
+if __name__ == '__main__':
+    pairs = ['XZECXXBT, XZECZEUR, XXBTZEUR, XETHXXBT, XETHZEUR']
+    table, table_headers = get_ticker(pairs)
+    sorted_table = sorted(table, key=lambda x: x[-1], reverse=True)
+    import tabulate
     print(tabulate.tabulate(sorted_table, headers=table_headers))
