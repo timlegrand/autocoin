3cbaa1d2da6f55baf2a700e2640c75ca8ae921e7
Tim Legrand <timlegrand.perso+dev@gmail.com>
2017-08-09 23:28:44 +0200

    refactor(server time): insulate queries into a dedicated function

--- src/data/server-time.py
+++ src/data/server-time.py
@@ -1,10 +1,14 @@
 from connectors import request
-import json
-import datetime
+
+
+def get_server_time():
+    response = request.request('server time')
+    unixtime = response['unixtime']
+    return unixtime
 
 
 if __name__ == '__main__':
-    response = request.request('server time')
-    unixtime = response['unixtime']
+    unixtime = get_server_time()
+    import datetime
     time = datetime.datetime.fromtimestamp(int(unixtime)).strftime('%Y-%m-%d %H:%M:%S')
     print(time)
