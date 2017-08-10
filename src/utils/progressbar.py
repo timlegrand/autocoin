2057efca25ebd3c0367b0382d8394ad62e843a4d
Tim Legrand <timlegrand.perso+dev@gmail.com>
2017-08-11 00:24:11 +0200

    feat: add a optional message to progressbar

--- src/utils/progressbar.py
+++ src/utils/progressbar.py
@@ -3,13 +3,13 @@ import sys
 progress_length = 100
 
 
-def print(progress_percents, size=100):
+def print(progress_percents, size=100, msg=''):
     global progress_length
     last_progress_char = '>' if progress_percents < 100 else '='
     progress_char_number = (progress_percents - 1) * size // 100
     remaining_char_number = (100 - progress_percents) * size // 100
     new_line = '\n' if progress_percents == 100 else ''
-    progress_line = '[' + progress_char_number*'=' + last_progress_char + remaining_char_number*'-' + '] ' + str(progress_percents) + '%'
+    progress_line = msg + '[' + progress_char_number*'=' + last_progress_char + remaining_char_number*'-' + '] ' + str(progress_percents) + '%'
     progress_length = len(progress_line)
     sys.stdout.write(progress_line + new_line)
     sys.stdout.flush()
