5266bb5730e4a0eadf314a3c974b2e62da463ef2
Tim Legrand <timlegrand.perso+dev@gmail.com>
2017-08-13 01:56:51 +0200

    feat(ticker): return ticker as dict instead of list

--- src/data/ticker.py
+++ src/data/ticker.py
@@ -4,12 +4,11 @@ from connectors import request
 def get_ticker(pairs=['XXBTZEUR']):
     ticker = request.request('ticker', {'pair': ', '.join(pairs)})
     
-    table=[]
+    table={}
     for k, v in ticker.items():
         bid = float(v['b'][0])
         ask = float(v['a'][0])
-        ticker_line = [k, bid, ask]
-        table.append(ticker_line)
+        table[k] = (bid, ask)
 
     table_headers=['Pair', 'Bid', 'Ask']
 
@@ -19,6 +18,6 @@ def get_ticker(pairs=['XXBTZEUR']):
 if __name__ == '__main__':
     pairs = ['XZECXXBT, XZECZEUR, XXBTZEUR, XETHXXBT, XETHZEUR']
     table, table_headers = get_ticker(pairs)
-    sorted_table = sorted(table, key=lambda x: x[-1], reverse=True)
+    sorted_table = sorted(table.items(), key=lambda x: x[1], reverse=True)
     import tabulate
     print(tabulate.tabulate(sorted_table, headers=table_headers))
