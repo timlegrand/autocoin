ce3b5317599139b89b63475b0a1318a7a025f89f
Tim Legrand <timlegrand.perso+dev@gmail.com>
2017-08-14 00:04:13 +0200

    feat(assets): add a function to get real asset name from altname

--- src/data/assets.py
+++ src/data/assets.py
@@ -14,7 +14,23 @@ def get_assets():
     return assets
 
 
+def get_asset_standard_name(name_or_altname, assets_dict=None):
+    '''Returns the standard name of a given asset.
+    Given asset may be provided in both standard or common forms.'''
+    if not assets_dict:
+        assets_dict = get_assets()
+
+    if name_or_altname in assets_dict:
+        return name_or_altname
+
+    for asset_name, info in assets_dict.items():
+        if info['altname'] == name_or_altname:
+            return asset_name
+
+
 if __name__ == '__main__':
     asset_pairs = get_asset_pairs()
     print(asset_pairs)
     assets_dict = get_assets()
+    print('LTC is ' + get_asset_standard_name('LTC', assets_dict))
+    print('XETH is ' + get_asset_standard_name('XETH', assets_dict))
