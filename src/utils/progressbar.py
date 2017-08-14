d143bcac80f2559db8001856dbc6fffa2df66c29
Tim Legrand <timlegrand.perso+dev@gmail.com>
2017-08-15 01:06:34 +0200

    fix(progressbar): prevent size change while inscrease

--- src/utils/progressbar.py
+++ src/utils/progressbar.py
@@ -6,19 +6,19 @@ progress_length = 100
 
 def update(progress_percents, size=15, msg=''):
     global progress_length
-    last_char = '-'
+    current_char = '-'
     if progress_percents < 100 and progress_percents > 0:
-        last_char = '>'
+        current_char = '>'
     elif progress_percents == 100:
-        last_char = '='
-    progress_char_number = (progress_percents - 1) * size // 100
-    remaining_char_number = (100 - progress_percents) * size // 100
+        current_char = '='
+    progress_char_number = progress_percents * size // 100
+    remaining_char_number = size - progress_char_number
     new_line = '\n' if progress_percents == 100 else ''
     if msg: msg += ' '
     progress_line = '{:25} [{}{}{}] {}%'.format(
         msg,
         progress_char_number*'=',
-        last_char,
+        current_char,
         remaining_char_number*'-',
         str(progress_percents))
     progress_length = len(progress_line)
