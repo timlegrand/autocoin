71c795e2dd99b10adead8eea8d9cdb00c9963de0
Tim Legrand <timlegrand.perso+dev@gmail.com>
2017-08-14 23:56:11 +0200

    feat(assets): add caching capabilities to assets and asset pairs requests

--- src/data/assets.py
+++ src/data/assets.py
@@ -1,8 +1,19 @@
 from connectors import request
+from utils import progressbar
+
+
+cache_asset_pairs = None
+cache_assets = None
 
 
 def get_asset_pairs():
-    asset_pairs = request.request('asset pairs')
+    global cache_asset_pairs
+    progressbar.print(0, size=10, msg='Downloading asset pairs')
+    if not cache_asset_pairs:
+        cache_asset_pairs = request.request('asset pairs')
+    asset_pairs = cache_asset_pairs
+    progressbar.erase()
+    progressbar.print(100, size=10, msg='Downloading asset pairs')
     for pair in list(asset_pairs):
         if pair[-2:] == '.d':
             del asset_pairs[pair]
@@ -10,7 +21,13 @@ def get_asset_pairs():
 
 
 def get_assets():
-    assets = request.request('assets')
+    global cache_assets
+    progressbar.print(0, size=10, msg='Downloading assets')
+    if not cache_assets:
+        cache_assets = request.request('assets')
+    assets = cache_assets
+    progressbar.erase()
+    progressbar.print(100, size=10, msg='Downloading assets')
     return assets
 
 
